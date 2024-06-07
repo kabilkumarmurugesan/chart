497db10858a02bf1bb94b7000572c288e530cb3f
Teebanraj <teebanraj.dhanusu@twilightsoftwares.com>
2024-06-07 11:51:20 +0530

    lenova.

--- src/component/Layout/AppContainer.jsx
+++ src/component/Layout/AppContainer.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from "react";
-import BarChart from "../charts/BarChart";
-import { Box, Card, Grid, Typography, useTheme } from "@mui/material";
-import BasicTable from "../Table/Table";
-import BasicAction from "../Card/BasicAction";
-import BarChartCopy from "../charts/BarChartCopy";
-import { CSSTransition, SwitchTransition } from "react-transition-group";
-import SingleChart from "../charts/SingleChart";
+import React, { useEffect, useState } from 'react';
+import BarChart from '../charts/BarChart';
+import { Box, Card, Grid, Typography, useTheme } from '@mui/material';
+import BasicTable from '../Table/Table';
+import BasicAction from '../Card/BasicAction';
+import BarChartCopy from '../charts/BarChartCopy';
+import { CSSTransition, SwitchTransition } from 'react-transition-group';
+import SingleChart from '../charts/SingleChart';
 
 const AppContainer = () => {
   const theme = useTheme();
   const { primary } = theme.palette;
   const [currentSlide, setCurrentSlide] = useState(0);
+  let yesterday = new Date(new Date().setDate(new Date().getDate() - 1));
+  let dateString = yesterday.toLocaleDateString('en-US'); // MM/DD/YYYY in US English
+  const [yseterdate, setYestrdate] = useState(dateString);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -18,6 +21,13 @@ const AppContainer = () => {
     }, 4000);
     return () => clearInterval(interval);
   }, []);
+
+  const formatDate = (date) => {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() is zero-based
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  };
 
   return (
     <SwitchTransition>
@@ -29,101 +39,101 @@ const AppContainer = () => {
       >
         <Box
           className="zoom-fade-container"
-          sx={{ background: primary.main, fontWeight: "bold", height: "100vh" }}
+          sx={{ background: primary.main, fontWeight: 'bold', height: '100vh' }}
         >
           {currentSlide === 0 ? (
             <Box sx={{ p: 2 }}>
               <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
                   <Card sx={{ minWidth: 275 }}>
-                    {" "}
+                    {' '}
                     <Typography
                       sx={{ p: 1 }}
                       style={{
-                        fontFamily: "cursive",
-                        fontSize: "20px",
+                        fontFamily: 'cursive',
+                        fontSize: '20px',
                       }}
                     >
-                      Date - 29/05/2024
-                    </Typography>{" "}
+                      Date -{yseterdate}
+                    </Typography>{' '}
                     <hr />
                     <Box
                       sx={{
-                        display: "flex",
-                        flexDirection: "row",
+                        display: 'flex',
+                        flexDirection: 'row',
                         p: 1,
-                        alignItems: "center",
-                        justifyContent: "space-between",
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
                       }}
                     >
-                      {" "}
+                      {' '}
                       <Typography
                         sx={{ p: 1 }}
                         style={{
-                          fontFamily: "cursive",
-                          fontSize: "15px",
+                          fontFamily: 'cursive',
+                          fontSize: '15px',
                         }}
                       >
                         Time - 09:00 AM - 05:30PM (shift 2)
                       </Typography>
-                      <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
                         <Typography
                           sx={{ p: 1 }}
                           style={{
-                            fontFamily: "cursive",
-                            fontSize: "15px",
+                            fontFamily: 'cursive',
+                            fontSize: '15px',
                           }}
                         >
                           OverTime 07:30PM
                         </Typography>
                       </Box>
-                    </Box>{" "}
+                    </Box>{' '}
                     <hr />
                     <BarChart />
                   </Card>
                 </Grid>
                 <Grid item xs={12} md={6}>
                   <Card sx={{ minWidth: 275 }}>
-                    {" "}
+                    {' '}
                     <Typography
                       sx={{ p: 1 }}
                       style={{
-                        fontFamily: "cursive",
-                        fontSize: "20px",
+                        fontFamily: 'cursive',
+                        fontSize: '20px',
                       }}
                     >
-                      Date - 30/05/2024
+                      Date -{formatDate(new Date())}
                     </Typography>
                     <hr />
                     <Box
                       sx={{
-                        display: "flex",
-                        flexDirection: "row",
+                        display: 'flex',
+                        flexDirection: 'row',
                         p: 1,
-                        alignItems: "center",
-                        justifyContent: "space-between",
+                        alignItems: 'center',
+                        justifyContent: 'space-between',
                       }}
                     >
                       <Typography
                         sx={{ p: 1 }}
                         style={{
-                          fontFamily: "cursive",
-                          fontSize: "15px",
+                          fontFamily: 'cursive',
+                          fontSize: '15px',
                         }}
                       >
                         Time - 09:00 AM - 05:30PM(shift 1)
                       </Typography>
-                      <Box sx={{ display: "flex", flexDirection: "row" }}>
+                      <Box sx={{ display: 'flex', flexDirection: 'row' }}>
                         <Typography
                           sx={{ p: 1 }}
                           style={{
-                            fontFamily: "cursive",
-                            fontSize: "15px",
+                            fontFamily: 'cursive',
+                            fontSize: '15px',
                           }}
                         >
                           OverTime 07:30PM
                         </Typography>
-                      </Box>{" "}
+                      </Box>{' '}
                     </Box>
                     <hr />
                     <BarChartCopy animations={false} />
@@ -156,7 +166,7 @@ const AppContainer = () => {
                       animations={{
                         tension: {
                           duration: 10000,
-                          easing: "linear",
+                          easing: 'linear',
                           from: 1,
                           to: 0,
                         },
@@ -165,11 +175,7 @@ const AppContainer = () => {
                   </Card>
                 </Grid>
                 <Grid item xs={5} md={4}>
-                  <Grid
-                    container
-                    spacing={4}
-                    // columnSpacing={{ xs: 2, sm: 2, md: 3 }}
-                  >
+                  <Grid container spacing={4}>
                     <Grid item xs={6} md={6}>
                       <Card sx={{ minWidth: 275, padding: 2 }}>
                         <Box
@@ -180,16 +186,16 @@ const AppContainer = () => {
                           <Box>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               Shift Actual
                             </Typography>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               15
@@ -198,7 +204,7 @@ const AppContainer = () => {
                           <img
                             src="./checklist.png"
                             alt="GIF"
-                            style={{ width: "60px", height: "60px" }}
+                            style={{ width: '60px', height: '60px' }}
                           />
                           {/*  <ListAltOutlinedIcon style={{ fontSize: '40px' }} /> */}
                         </Box>
@@ -214,16 +220,16 @@ const AppContainer = () => {
                           <Box>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               Shift Target
                             </Typography>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               15
@@ -232,7 +238,7 @@ const AppContainer = () => {
                           <img
                             src="./target.png"
                             alt="GIF"
-                            style={{ width: "60px", height: "60px" }}
+                            style={{ width: '60px', height: '60px' }}
                           />
                           {/*  <TrackChangesOutlinedIcon style={{ fontSize: '40px' }} /> */}
                         </Box>
@@ -248,16 +254,16 @@ const AppContainer = () => {
                           <Box>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               Shift UPH
                             </Typography>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               15
@@ -266,7 +272,7 @@ const AppContainer = () => {
                           <img
                             src="./presentation.png"
                             alt="GIF"
-                            style={{ width: "60px", height: "60px" }}
+                            style={{ width: '60px', height: '60px' }}
                           />
                           {/*  <NorthOutlinedIcon style={{ fontSize: '40px' }} /> */}
                         </Box>
@@ -282,16 +288,16 @@ const AppContainer = () => {
                           <Box>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               Shift Down Time
                             </Typography>
                             <Typography
                               style={{
-                                fontFamily: "cursive",
-                                fontSize: "20px",
+                                fontFamily: 'cursive',
+                                fontSize: '20px',
                               }}
                             >
                               15
@@ -300,7 +306,7 @@ const AppContainer = () => {
                           <img
                             src="./hourglass.png"
                             alt="GIF"
-                            style={{ width: "60px", height: "60px" }}
+                            style={{ width: '60px', height: '60px' }}
                           />
                           {/*     <UpdateOutlinedIcon style={{ fontSize: '40px' }} /> */}
                         </Box>
--- src/component/Layout/AppHeader.jsx
+++ src/component/Layout/AppHeader.jsx
@@ -5,9 +5,9 @@ import {
   Toolbar,
   Typography,
   useTheme,
-} from "@mui/material";
-import React from "react";
-import { MUIWrapperContext } from "../MUIWrapper";
+} from '@mui/material';
+import React from 'react';
+import { MUIWrapperContext } from '../MUIWrapper';
 
 export default function AppHeader() {
   const theme = useTheme();
@@ -18,24 +18,49 @@ export default function AppHeader() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="default">
         <Toolbar sx={{ height: 80 }}>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}
-          style={{fontFamily:"sans-serif",textAlign:"center", fontStyle:"oblique", fontWeight:"bold"}}>
-            LENOVA 24Hr UPH DASHBOARD
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            style={{
+              fontFamily: 'sans-serif',
+              textAlign: 'center',
+              fontStyle: 'oblique',
+              fontWeight: 'bold',
+            }}
+          >
+            LENOVO 24Hr UPH DASHBOARD
           </Typography>
 
           <IconButton
-            sx={{ fontSize: "1rem" }}
+            sx={{ fontSize: '1rem' }}
             onClick={toggleColorMode}
             color="inherit"
             disableTouchRipple
             disableRipple
           >
-            {theme.palette.mode === "dark" ? (
-              <span role="img" aria-label="sun" style={{fontFamily:"sans-serif",textAlign:"center", fontStyle:"oblique"}}>
+            {theme.palette.mode === 'dark' ? (
+              <span
+                role="img"
+                aria-label="sun"
+                style={{
+                  fontFamily: 'sans-serif',
+                  textAlign: 'center',
+                  fontStyle: 'oblique',
+                }}
+              >
                 Light ☀️
               </span>
             ) : (
-              <span role="img" aria-label="moon" style={{fontFamily:"sans-serif",textAlign:"center", fontStyle:"oblique"}}>
+              <span
+                role="img"
+                aria-label="moon"
+                style={{
+                  fontFamily: 'sans-serif',
+                  textAlign: 'center',
+                  fontStyle: 'oblique',
+                }}
+              >
                 Dark 🌚
               </span>
             )}
