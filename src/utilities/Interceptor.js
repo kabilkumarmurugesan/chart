bb30c5aab0b2f72bb6623825dd1f27203429b168
vengatavaradhanp <77961420+vengatavaradhanp@users.noreply.github.com>
2024-07-01 20:05:31 +0530

    end point.

--- src/utilities/Interceptor.js
+++ src/utilities/Interceptor.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8001/api/v1/general";
+// axios.defaults.baseURL = "http://localhost:8001/api/v1/general";
+
+axios.defaults.baseURL = "http://137.184.206.139:8001/api/v1/general";
 
 const AxiosInstances = axios.create({
   headers: {
